a1d334a8db4da5db6bd2b53cb6070ed2431b0f4b
DaniyalAkhtar <daniyalakhtarpatoojo125@gmail.com>
2026-08-13 11:46:52 +0500

    Cupertino Icons Added

--- lib/repo/bottomnavigator/bottomnavigatorscreen.dart
+++ lib/repo/bottomnavigator/bottomnavigatorscreen.dart
@@ -2,7 +2,7 @@ import 'package:chatapp/repo/chats/chats_screen.dart';
 import 'package:chatapp/repo/contacts/contacts_screen.dart';
 import 'package:chatapp/repo/more/more_screen.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/cupertino.dart';
 class Bottomnavigatorscreen extends StatefulWidget {
   const Bottomnavigatorscreen({super.key});
 
@@ -21,8 +21,8 @@ class _BottomnavigatorscreenState extends State<Bottomnavigatorscreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Contacts'),
-        BottomNavigationBarItem(icon: Icon(Icons.chat_bubble),label: 'Chats'),
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.person_2),label: 'Contacts'),
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.chat_bubble_2),label: 'Chats'),
         BottomNavigationBarItem(icon: Icon(Icons.more_horiz),label: 'More'),
       ],currentIndex: currentindex,
         onTap: (value){
